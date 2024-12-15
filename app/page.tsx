d7f3e535499b93aecb6f1c3da77062ca08036a42
Kevyn Franco <kevyn@franco.expert>
2024-12-15 15:08:48 -0500

    feat update

--- app/page.tsx
+++ app/page.tsx
@@ -26,26 +26,37 @@ export default function Home() {
                         `,
                     }}
                 />
+                {/* Twitter Conversion Tracking Script */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+                            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+                            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+                            twq('config','ov3qk');
+                        `,
+                    }}
+                />
             </Head>
             <noscript>
                 <iframe
                     src="https://www.googletagmanager.com/ns.html?id=GTM-WV3GTG44"
                     height="0"
                     width="0"
-                    style={{ display: "none", visibility: "hidden" }}
+                    style={{display: "none", visibility: "hidden"}}
                 ></iframe>
             </noscript>
 
             {/* App Layout */}
             <Suspense>
-                <Header />
+                <Header/>
             </Suspense>
             <main>
-                <Hero />
-                <Problem />
-                <WithWithout />
-                <FeaturesAccordion />
-                <Pricing />
+                <Hero/>
+                <Problem/>
+                <WithWithout/>
+                <FeaturesAccordion/>
+                <Pricing/>
                 <Testimonials3 />
                 <FAQ />
             </main>
